24787e37f245e215aa57b69ad1af31eeea402cfc
Keunbae Park <ppotpo@gmail.com>
2019-09-09 22:38:25 -0400

    wip

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+        factory(User::class)->create([
+            'name' => 'a',
+            'email' => 'a@a.com',
+            'password' => bcrypt('a')
+        ]);
+    }
+}
