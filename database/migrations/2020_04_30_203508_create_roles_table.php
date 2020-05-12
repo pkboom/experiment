7a46abbfde0938b3150e92258c992e2ef5b7db4a
Keunbae Park <ppotpo@gmail.com>
2020-05-12 14:46:01 -0400

    wip

--- database/migrations/2020_04_30_203508_create_roles_table.php
+++ database/migrations/2020_04_30_203508_create_roles_table.php
@@ -20,4 +20,10 @@ class CreateRolesTable extends Migration
             $table->timestamps();
         });
     }
+
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('role_user');
+    }
 }
