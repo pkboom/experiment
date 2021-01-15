b0fc2f6d32a23a3b8d38d579d1e55b3cb9c7cc92
Keunbae Park <ppotpo@gmail.com>
2021-01-15 16:31:53 -0500

    wip

--- database/migrations/2021_01_09_184505_create_posts_table.php
+++ database/migrations/2021_01_09_184505_create_posts_table.php
@@ -1,31 +1,20 @@
 <?php
 
+use App\Enums\StatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreatePostsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->enum('status', StatusEnum::toValues());
+            $table->enum('nullable_enum', StatusEnum::toValues())->nullable();
+            $table->json('array_of_enums');
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('posts');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Enums\StatusEnum;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -19,7 +19,25 @@ use Inertia\Inertia;
 // })->name('dashboard');
 
 Route::get('/', function () {
-    dd(StatusEnum::DRAFT());
+    $response = Http::withOptions([
+        'base_uri' => 'http://api.sportradar.us',
+        'stream' => true,
+    ])->get(
+        // '/nba/simulation/v7/en/games/2017/SIM/schedule.json?api_key=xu6cdvtaszrbkme87jgt8jjv' // simulation data
+        // '/nba/simulation/v7/en/games/12b2e554-c80f-404f-b140-c25400996ac7/summary.json?api_key=xu6cdvtaszrbkme87jgt8jjv' // game summary
+        // '/nba/simulation/stream/en/events/subscribe?api_key=xu6cdvtaszrbkme87jgt8jjv&sd:match:12b2e554-c80f-404f-b140-c25400996ac7' // stream data for a game
+        // '/nba/simulation/stream/en/events/subscribe?api_key=xu6cdvtaszrbkme87jgt8jjv&sd:team:583ecd4f-fb46-11e1-82cb-f4ce4684ea4c' // stream data for a team
+        // '/nba/simulation/v7/en/games/12b2e554-c80f-404f-b140-c25400996ac7/pbp.json?api_key=xu6cdvtaszrbkme87jgt8jjv'
+        // '/nhl/simulation/v7/en/games/2017/SIM/schedule.json?api_key=nbqpwfg6z6kv4xezfnb46dnu' // simulation data
+        '/nhl/simulation/v7/en/games/0cdfbafa-ee01-415f-85d5-9b7c7fb3af71/summary.json?api_key=nbqpwfg6z6kv4xezfnb46dnu' // game summary
+        // '/nhl/simulation/stream/en/clock/subscribe?api_key=nbqpwfg6z6kv4xezfnb46dnu'
+        // '/nhl/simulation/stream/en/events/subscribe?api_key=nbqpwfg6z6kv4xezfnb46dnu&sd:match:0cdfbafa-ee01-415f-85d5-9b7c7fb3af71'
+      );
 
-    return;
+    $body = $response->getBody();
+
+    return $body;
+    while (!$body->eof()) {
+        dump($body->read(128));
+    }
 });
