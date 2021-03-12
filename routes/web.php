5d7f28aeb938439d82661e526c594f9356dbf464
Keunbae Park <ppotpo@gmail.com>
2021-03-12 01:50:39 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Symfony\Component\Yaml\Yaml;
+use Laravel\Socialite\Facades\Socialite;
 
 // Route::get('/', function () {
 //     return Inertia::render('Welcome', [
@@ -19,3 +19,13 @@ use Symfony\Component\Yaml\Yaml;
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return Inertia::render('Dashboard');
 // })->name('dashboard');
+
+Route::get('/', function (Request $request) {
+    // $request->session()->put('state', 'foooooooooo');
+    // $request->session()->regenerate();
+    // dump(get_class($request->session()));
+    // dump($request->session()->all());
+    // dump($request->session()->get('state', 'goooooooooo'));
+    // Socialite::driver('github')->redirect();
+    return now();
+});
