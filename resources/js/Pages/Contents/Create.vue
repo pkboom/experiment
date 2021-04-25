6a4bc257f3429ca91127152818f3edb6c4695e3f
Keunbae Park <ppotpo@gmail.com>
2021-04-24 22:33:48 -0400

    wip

---
+++ resources/js/Pages/Contents/Create.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    course: String,
+    section: String,
+    elearnings: String,
+  },
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {},
+}
+</script>
---
+++ resources/js/Pages/Contents/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+		course: String,
+		section: String,
+		elearnings: String,
+    }, 
+    data() {
+    return {}
+    },
+    mounted() {},
+    methods: {},
+}
+</script>
