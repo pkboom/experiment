1415ad9921b6a53665986deec3a7ab38d0fbb6e9
Keunbae Park <ppotpo@gmail.com>
2021-05-16 04:18:36 -0400

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Spatie\Sheets\Facades\Sheets;
 
 // Route::get('/', function () {
 //     return Inertia::render('Welcome', [
@@ -18,12 +17,3 @@ use Spatie\Sheets\Facades\Sheets;
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return Inertia::render('Dashboard');
 // })->name('dashboard');
-
-Route::get('/', function () {
-    $repository = Sheets::collection('posts');
-    $helloWorld = Sheets::collection('posts')->get('2021-05-05.hello-world');
-    dump($helloWorld);
-    dump($repository->all());
-
-    return $repository->all();
-});
