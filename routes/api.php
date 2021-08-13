790dd2a1b40741a0ada77c4797e60554f692091e
Keunbae Park <ppotpo@gmail.com>
2021-08-13 16:49:27 -0400

    wip

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('webhook', function () {
     Log::info(Request::all());
+    Log::info(Request::header());
 });
