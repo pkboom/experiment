3f3c8f2501a1557ce722bf95224941363850fbcb
Keunbae Park <ppotpo@gmail.com>
2021-08-13 16:45:16 -0400

    wip

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\UserController;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,3 +13,7 @@ Route::get('/', function () {
 
 Route::get('messages', MessageController::class);
 Route::get('users', [UserController::class, 'index']);
+
+Route::get('webhook', function () {
+    Log::info(Request::all());
+});
