61c84c60fc08721637269fdcc082477889d22027
Keunbae Park <ppotpo@gmail.com>
2022-01-05 14:12:01 -0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,15 @@ Route::get('/', function (Request $request) {
     //     })->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
     // file_put_contents(storage_path('pt.json'), $json);
+
+    $json = SimpleExcelReader::create(storage_path('portuguese.xlsx'))->getRows()
+        ->filter(function ($rowProperties) {
+            return trim($rowProperties['Drug_class']) !== trim($rowProperties['Classe_da_Droga']);
+        })
+        ->unique()
+        ->mapWithKeys(function (array $rowProperties) {
+            return [trim($rowProperties['Drug_class']) => trim($rowProperties['Classe_da_Droga'])];
+        })->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+
+    file_put_contents(storage_path('pt.json'), $json);
 });
