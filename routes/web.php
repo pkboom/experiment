8d2325d0f43f6f320be7fe1cfdddae7fbf14e635
Keunbae Park <pkboom@users.noreply.github.com>
2024-09-29 12:16:56 -0400

    wip

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', fn () => Inertia::render('Register'));
+Route::view('/', 'welcome');
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome');
-// });
+// Route::get('/', fn () => Inertia::render('Register'));
