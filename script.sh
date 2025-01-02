81332cc3327cbe7bfc18f8f1b3a6236a0b27060f
Keunbae Park <pkboom@users.noreply.github.com>
2025-01-01 22:49:56 -0500

    wip

--- script.sh
+++ script.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
+
 PATH=./node_modules/.bin:$PATH
 
-function composer_asdf {
-  composer update
+composer_asdf() {
+    composer update
 }
 
-function npm_asdf {
-  npm ci
+npm_asdf() {
+    npm ci
 }
 
-function start {
-  echo 'start'
+start() {
+    echo 'start'
 }
 
-function default {
-  start
+default() {
+    start
 }
 
-function help {
-  echo "$0 <task> <args>"
-  echo "Tasks:"
-  compgen -A function | cat -n
+help() {
+    echo "$0 <task> <args>"
+    echo "Tasks:"
+    compgen -A function | cat -n
 }
 
 TIMEFORMAT="Task completed in %3lR"
 time ${@:-default}
 
-compgen -A function
+# compgen -A function
